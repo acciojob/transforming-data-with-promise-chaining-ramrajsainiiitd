515f54732133639c75123ba4c22e6a55ea19a982
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-18 13:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 //your JS code here. If required.
+let input = document.getElementById('ip');
+let start = document.getElementById('btn');
+let output = document.getElementById('output');
+
+start.addEventListener("click",()=>{
+  let promise1 = new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve(Number(input.value));
+    },2000);
+  });
+
+  promise1.then((num)=>{
+    output.textContent = `Result: ${num}`;
+    return num;
+  }).then((num)=>{
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve(num*2);
+      }, 1000);
+    });
+  }).then((num)=>{
+    output.textContent = `Result: ${num}`;
+    return num;
+  }).then((num)=>{
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve(num-3);
+      }, 1000);
+    });
+  })
+    .then((num)=>{
+      output.textContent = `Result: ${num}`;
+      return num;
+    })
+    // promise 4
+    .then((num)=>{
+      return new Promise((resolve)=>{
+        resolve(num/2);
+      }, 1000);
+    }).then((num)=>{
+      output.textContent = `Result: ${num}`;
+      return num;
+    })
+    // promise 5
+    .then((num)=>{
+      return new Promise((resolve)=>{
+        resolve(num + 10);
+      }, 1000);
+    }).then((num)=>{
+      output.textContent = `Result: ${num}`;
+    }).catch((e)=>{
+console.log(e);
+    })
+});
