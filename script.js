7adef2c8aa52f46020ea872f63f8f679f41bb4da
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-23 05:38:36 +0000

    timing

--- script.js
+++ script.js
@@ -2,56 +2,61 @@ let input = document.getElementById('ip');
 let start = document.getElementById('btn');
 let output = document.getElementById('output');
 
-start.addEventListener("click",()=>{
-  let promise1 = new Promise((resolve)=>{
-    setTimeout(()=>{
-      resolve(Number(input.value));
-    },2000);
-  });
+start.addEventListener("click", () => {
+    let promise1 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(Number(input.value));
+        }, 2000);
+    });
 
-  promise1.then((num)=>{
-    output.textContent = `Result : ${num}`;
-    return num;
-  })
-  .then((num)=>{
-    return new Promise((resolve)=>{
-      setTimeout(()=>{
-        resolve(num*2);
-      }, 1000);
+    promise1.then((num) => {
+        output.textContent = `Result: ${num}`;
+        return num;
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(num * 2);
+            }, 2000);
+        });
+    })
+    .then((num) => {
+        output.textContent = `Result: ${num}`;
+        return num;
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(num - 3);
+            }, 1000);
+        });
+    })
+    .then((num) => {
+        output.textContent = `Result: ${num}`;
+        return num;
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(num / 2);
+            }, 1000);
+        });
+    })
+    .then((num) => {
+        output.textContent = `Result: ${num}`;
+        return num;
+    })
+    .then((num) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(num + 10);
+            }, 1000);
+        });
+    })
+    .then((num) => {
+        output.textContent = `Result: ${num}`;
+    })
+    .catch((error) => {
+        console.error(error);
     });
-  }).then((num)=>{
-    output.textContent = `Result : ${num}`;
-    return num;
-  })
-  .then((num)=>{
-    return new Promise((resolve)=>{
-      setTimeout(()=>{
-        resolve(num-3);
-      }, 1000);
-    });
-  }).then((num)=>{
-    output.textContent = `Result : ${num}`;
-    return num;
-  })
-  .then((num)=>{
-    return new Promise((resolve)=>{
-      setTimeout(()=>{
-        resolve(num/2);
-      }, 1000);
-    });
-  }).then((num)=>{
-    output.textContent = `Result : ${num}`;
-    return num;
-  })
-  .then((num)=>{
-    return new Promise((resolve)=>{
-      setTimeout(()=>{
-        resolve(num + 10);
-      }, 1000);
-    });
-  }).then((num)=>{
-    output.textContent = `Result : ${num}`;
-  }).catch((e)=>{
-    console.log(e);
-  })
 });
